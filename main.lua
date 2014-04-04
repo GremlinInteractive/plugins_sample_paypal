3e1131b3286fd676318cfae484ff16c8c2851927
Daniel Glover <dcgm1986@gmail.com>
2014-04-04 15:20:10 +0100

    - Adding PayPal Plugin sample code.

---
+++ main.lua
@@ -0,0 +1,213 @@
+--
+-- Abstract: Paypal plugin sample app
+--
+-- Version: 1.0
+--
+-- Sample code is MIT licensed, see http://www.coronalabs.com/links/code/license
+-- Copyright (C) 2014 Gremlin Interactive™. All Rights Reserved.
+--
+
+-- Related PayPal documentation
+--[[
+https://github.com/paypal/PayPal-iOS-SDK
+https://github.com/paypal/PayPal-iOS-SDK/blob/master/README.md
+https://github.com/paypal/PayPal-iOS-SDK/blob/master/docs/future_payments_mobile.md
+https://github.com/paypal/PayPal-iOS-SDK/blob/master/docs/future_payments_server.md#obtain-oauth2-tokens
+https://developer.paypal.com/webapps/developer/docs/integration/mobile/make-future-payment/#required-best-practices-for-future-payments
+https://github.com/paypal/PayPal-iOS-SDK/blob/master/docs/single_payment.md
+https://developer.paypal.com/webapps/developer/docs/integration/mobile/verify-mobile-payment/
+--]]
+
+--[[
+-- Images used in this sample code are sourced from www.openclipart.org
+Background image: http://openclipart.org/detail/191047/background-design-by-anarres-191047
+T-Shirt image: http://openclipart.org/detail/177398/blue-polo-shirt-remix-by-merlin2525-177398
+Bus: http://openclipart.org/detail/182939/bus-1-by-Jarno-182939
+Button: http://openclipart.org/detail/26272/pill-button-yellow-by-anonymous
+--]]
+
+-- Require the Paypal library
+local paypal = require( "plugin.paypal" )
+local widget = require( "widget" )
+local json = require( "json" )
+
+-- Set the widget theme to iOS 6
+widget.setTheme( "widget_theme_ios" )
+
+-- Hide the status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+-- Initialize PayPal
+paypal.init
+{
+	licenseKey = "Insert_Your_License_Key_Here", -- If you are ready to use PayPals live servers, insert your license key here. To obtain a license for this plugin, please visit: http://gremlininteractive.com/product-category/corona-plugins/
+	testMode = true -- Change this to false if you are ready to use PayPals live servers. (i.e. you have set environment to "production" in paypal.config.
+}
+
+-- Configure PayPal
+paypal.config
+{
+	productionClientID = "Insert_Your_Production_Client_Id_Here",
+	sandboxClientID = "Insert_Your_Sandbox_Client_Id_Here",
+	acceptCreditCards = true, --true/false (optional) -- default false
+	language = "en", --The users language/locale -- If omitted paypal will show it's views in accordance with the device's current language setting.
+	merchant =
+	{
+		name = "Your Company Name", -- (required) -- The name of the merchant/company
+		privacyPolicyURL = "http://www.gremlininteractive.com", -- (optional) -- The merchants privacy policy url -- default is paypals privacy policy url
+		userAgreementURL = "http://www.gremlininteractive.com", -- (optional) -- The user agreement URL -- default is paypals user agreement url
+	},
+	rememberUser = false,
+	environment = "noNetwork", -- Valid values: "sandbox", "noNetwork", "production"
+	-- Uncomment the below lines and fill in with your details, if required.
+	--[[
+	sandbox = 
+	{
+		useDefaults = true,
+		password = "Your_Sandbox_Password",
+		pin = "Your_Sandbox_Pin",
+	},
+	user = 
+	{
+		email = "User_Paypal_Email",
+		phoneNumber = "User_Phone_Number",
+		phoneCountryCode = "User_Phone_Country_Code",
+	},--]]
+}
+
+-- Display a background
+local background = display.newImageRect( "paypalBk.png", 320, 480 )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+
+-- Display the name of the sample code
+local sampleCodeNameText = display.newText
+{
+	text = "PayPal Plugin Sample Code",
+	font = native.systemFontBold,
+	fontSize = 18,
+}
+sampleCodeNameText.anchorX = 0.5
+sampleCodeNameText.anchorY = 0
+sampleCodeNameText.x = display.contentCenterX
+sampleCodeNameText.y = 10
+
+-- Display the t-shirt image
+local tShirt = display.newImageRect( "t-shirt.png", 120, 110 )
+tShirt.anchorX = 0.5
+tShirt.anchorY = 0
+tShirt.x = display.contentCenterX
+tShirt.y = 50
+
+-- Create some text to show the t-shirt name and price
+local tShirtDescriptionText = display.newText
+{
+	text = "Item: T-Shirt\nPrice: $50",
+	font = native.systemFont,
+	fontSize = 14,
+	width = 200,
+	align = "center",
+}
+tShirtDescriptionText.anchorX = 0.5
+tShirtDescriptionText.anchorY = 1
+tShirtDescriptionText.x = tShirt.x
+tShirtDescriptionText.y = tShirt.y + tShirt.contentHeight + tShirtDescriptionText.contentHeight
+
+-- Create the pay button
+local payButton = widget.newButton
+{
+	label = "Buy Now!",
+	onRelease = function( event )
+		paypal.show( "payment",
+		{
+			payment =
+			{
+				amount = 30.00,
+				tax = 10.00,
+				shipping = 10.00,
+				intent = "sale", -- Values "sale" and "authorize"
+			},
+			--bnCode = "XX",
+			currencyCode = "USD",
+			shortDescription = "Green T-Shirt",
+			--acceptCreditCards = false,
+			listener = function( event )
+				if event.response then
+					local confirmation = json.decode( event.response )
+					
+					for k, v in pairs( confirmation ) do
+						if type( v ) == "table" then
+							print( "table: {" )
+							for k1, v1 in pairs( v ) do
+								print( k1, ":", v1 )
+							end
+							print( "}" )
+						else
+							print( k, ":", v )
+						end
+					end
+				else
+					for k, v in pairs( event ) do
+						print( k, ":", v )
+					end
+				end
+			end,
+		})
+	end,
+}
+payButton.x = display.contentCenterX
+payButton.y = tShirtDescriptionText.y + tShirtDescriptionText.contentHeight
+
+
+-- Display the bus image
+local bus = display.newImage( "bus.png", 213, 110 )
+bus.x = display.contentCenterX
+bus.y = payButton.y + payButton.contentHeight + ( bus.contentHeight * 0.5 ) - 10
+
+-- Create some text to show the bus name and price
+local busDescriptionText = display.newText
+{
+	text = "Item: Bus Ticket Auto Pass\nPrice: $5 Per Trip",
+	font = native.systemFont,
+	fontSize = 14,
+	width = 200,
+	align = "center",
+}
+busDescriptionText.anchorX = 0.5
+busDescriptionText.anchorY = 1
+busDescriptionText.x = bus.x
+busDescriptionText.y = bus.y + bus.contentHeight - 18
+
+-- Create a button to show a future payment window
+local futurePaymentButton = widget.newButton
+{
+	label = "View Agreement",
+	onRelease = function( event )
+		paypal.show( "futurePayment",
+		{
+			listener = function( event )
+				if event.response then
+					local confirmation = json.decode( event.response )
+					
+					for k, v in pairs( confirmation ) do
+						if type( v ) == "table" then
+							print( "table: {" )
+							for k1, v1 in pairs( v ) do
+								print( k1, ":", v1 )
+							end
+							print( "}" )
+						else
+							print( k, ":", v )
+						end
+					end
+				else
+					for k, v in pairs( event ) do
+						print( k, ":", v )
+					end
+				end
+			end,
+		})
+	end,
+}
+futurePaymentButton.x = display.contentCenterX
+futurePaymentButton.y = busDescriptionText.y + busDescriptionText.contentHeight
